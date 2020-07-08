b29b5b4c2ddcb2f1b09995db0fa69db0ed4305bb
pj1301 <hayward.jp@gmail.com>
2020-07-08 17:08:56 +0100

    added mongo-db

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,18 @@
 import config from 'config';
 import express from 'express';
-import { setAppMiddleware } from './middleware/app-middleware';
+import { setAppMiddleware } from './middleware/appMiddleware';
 import { logger } from './utils/logger';
-
 
 export class App {
   private port: string = process.env.PORT || config.get('port');
   public app!: express.Application;
 
-  init() {
+  public async init() {
     this.app = express();
     setAppMiddleware(this.app);
   }
 
-  listen() {
+  public listen() {
     this.app.listen(this.port, () => logger.info(`Server running on port ${this.port}`));
   }
 }
---
+++ src/infrastructure/mongoConnection.ts
@@ -0,0 +1,17 @@
+import config from 'config';
+import { MongoClient, MongoClientOptions } from 'mongodb';
+
+export class MongoDatabaseClient {
+  public client!: MongoClient;
+  private mongoDbOpt: MongoClientOptions = { useUnifiedTopology: true };
+
+  public async getDatabaseClient(): Promise<MongoClient> {
+    const  client = new MongoClient(config.get('mongoDb.url'), this.mongoDbOpt);
+    return await client.connect();
+  }
+
+  public closeClient(): void {
+    this.client.close();
+  }
+}
+
