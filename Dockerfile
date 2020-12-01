e8f90ee121356e8ab4849a866a888ae779e53943
pj1301 <hayward.jp@gmail.com>
2020-12-01 03:22:45 +0000

    working server

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 FROM node:15.3.0
 
-WORKDIR /server_container
-ENV PORT=2000 NODE_ENV=development MONGO_URL=mongodb://mongo:27017
-RUN mkdir -p /server_container/node_modules && chown -R node:node /server_container
+WORKDIR /usr/server_container
+ENV PORT=2000 NODE_ENV=development MONGO_URL=mongodb://mongo:27017 PATH=./node_modules/.bin:$PATH
 COPY package*.json ./
-USER node
-RUN npm install
-COPY --chown=node:node . .
+RUN npm i && npm cache clean --force
+COPY . .
 EXPOSE 2000
-CMD ["npm", "start"]
+CMD nodemon ./src/server.ts
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,46 +1,74 @@
-import { NextFunction, Request, Response } from 'express';
-import { inject } from 'inversify';
-import {controller, httpPost, next, request, response} from 'inversify-express-utils';
-import TYPES from '../services/config/types';
-import { DatabaseService } from '../services/database.service';
-import { SecurityService } from '../services/security.service';
-import { TokenService } from '../services/token.service';
-import { UtilService } from '../services/util.service';
-import { InvalidCredentials } from '../utils/exceptions/credentials-exception';
+import { NextFunction, Request, Response } from "express";
+import { inject } from "inversify";
+import {
+	controller,
+	httpPost,
+	next,
+	request,
+	response,
+} from "inversify-express-utils";
+import TYPES from "../services/config/types";
+import { DatabaseService } from "../services/database.service";
+import { SecurityService } from "../services/security.service";
+import { TokenService } from "../services/token.service";
+import { UtilService } from "../services/util.service";
+import { InvalidCredentials } from "../utils/exceptions/credentials-exception";
 
-@controller('/auth')
+@controller("/auth")
 export class AuthController {
+	constructor(
+		@inject(TYPES.DatabaseService) private dbService: DatabaseService,
+		@inject(TYPES.TokenService) private tokenService: TokenService,
+		@inject(TYPES.SecurityService) private securityService: SecurityService,
+		@inject(TYPES.UtilService) private utilService: UtilService
+	) {}
 
-  constructor(
-    @inject(TYPES.DatabaseService) private dbService: DatabaseService,
-    @inject(TYPES.TokenService) private tokenService: TokenService,
-    @inject(TYPES.SecurityService) private securityService: SecurityService,
-    @inject(TYPES.UtilService) private utilService: UtilService
-  ) {}
+	@httpPost("/login")
+	public async login(
+		@request() req: Request,
+		@response() res: Response,
+		@next() nxt: NextFunction
+	): Promise<void> {
+		const user = await this.dbService.findOne("users", {
+			email: req.body.email,
+		});
+		if (!user) {
+			nxt(new InvalidCredentials());
+			return;
+		}
+		const checkedPw = await this.securityService.checkPw(
+			user.password,
+			req.body.password
+		);
+		if (!checkedPw) {
+			nxt(new InvalidCredentials());
+			return;
+		}
+		const token = this.tokenService.generateJWT(user._id, user.role);
+		token
+			? res.status(200).send({ token })
+			: nxt(new Error("An error occurred"));
+	}
 
-  @httpPost('/login')
-  public async login(@request() req: Request, @response() res: Response, @next() nxt: NextFunction) {
-    const user = await this.dbService.findOne('users', { email: req.body.email });
-    if (!user) return nxt(new InvalidCredentials());
-    const checkedPw = await this.securityService.checkPw(user.password, req.body.password);
-    if (!checkedPw) return nxt(new InvalidCredentials());
-    const token = this.tokenService.generateJWT(user._id, user.role);
-    res.status(200).send(token);
-  }
-
-  @httpPost('/register')
-  public async register(@request() req: Request, @response() res: Response, @next() nxt: NextFunction) {
-    const { email, password, role } = req.body;
-    // tailor validation as required
-    if (
-      !this.utilService.validateObject(email)
-      || !this.utilService.validateObject(password)
-      || !this.utilService.validateObject(role)
-    ) return nxt(new InvalidCredentials());
-    const encryptedPw = await this.securityService.encrypt(password);
-    const user = { email, password: encryptedPw, role };
-    const result = await this.dbService.createOne('users', user);
-    result ? res.status(200).send(result) : res.status(500).send({ message: 'bad request' });
-    res.status(200);
-  }
+	@httpPost("/register")
+	public async register(
+		@request() req: Request,
+		@response() res: Response,
+		@next() nxt: NextFunction
+	): Promise<void> {
+		const { email, password, role } = req.body;
+		// tailor validation as required
+		if (
+			!this.utilService.validateObject(email) ||
+			!this.utilService.validateObject(password) ||
+			!this.utilService.validateObject(role)
+		) {
+			nxt(new InvalidCredentials());
+			return;
+		}
+		const encryptedPw = await this.securityService.encrypt(password);
+		const user = { email, password: encryptedPw, role };
+		const result = await this.dbService.createOne("users", user);
+		result ? res.status(200).send(result) : nxt(new Error("Bad request"));
+	}
 }
